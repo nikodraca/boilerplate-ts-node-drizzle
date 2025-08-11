a09426b722220ceab3aff9dc4874bd40c6c2f3aa
Niko Draca <me@nikodraca.com>
2025-08-11 09:34:03 -0400

    some small quality of life improvements
    - remove axios
    - remove socket.io

--- src/gateways/spotifyGateway.ts
+++ src/gateways/spotifyGateway.ts
@@ -1,12 +1,22 @@
-import axios, { AxiosInstance } from 'axios';
-
 export class SpotifyGateway {
-  private gateway: AxiosInstance;
+  private baseURL = 'https://api.spotify.com';
+  private timeout = 3 * 1000;
 
-  constructor() {
-    this.gateway = axios.create({
-      baseURL: 'https://api.spotify.com',
-      timeout: 3 * 1000
-    });
+  private async request(endpoint: string, options: RequestInit = {}): Promise<Response> {
+    const controller = new AbortController();
+
+    const timeoutId = setTimeout(() => controller.abort(), this.timeout);
+
+    try {
+      const response = await fetch(`${this.baseURL}${endpoint}`, {
+        ...options,
+        signal: controller.signal
+      });
+      clearTimeout(timeoutId);
+      return response;
+    } catch (error) {
+      clearTimeout(timeoutId);
+      throw error;
+    }
   }
 }
